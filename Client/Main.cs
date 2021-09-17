c1b915b2828dab2678473a66dc24c78e0c238fa1
yukon <yukon@DESKTOP-I3TT191>
2021-09-17 10:58:23 +0900

    プロジェクト ファイルを追加します。

---
+++ Client/Main.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Reflection;
+using Photon.Realtime;
+using RubyButtonAPI;
+using MelonLoader;
+
+namespace Client
+{
+    public class Main : MelonMod
+    {
+        public override void OnApplicationStart()
+        {
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("      ____  _     _  ____  __  _  _____ \n    / (__`| |__ | || ===||   | ||_   _|\n     ____)|____||_||____||_| __|  |_|  ");
+            Console.ResetColor();
+            Console.WriteLine("Only for me");
+
+        }
+
+    }
+}
